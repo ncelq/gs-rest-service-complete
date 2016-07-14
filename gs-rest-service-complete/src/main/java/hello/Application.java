2d121ca21a034b9fe2bf7dc0d91df504cefd51e9
ali <ali@ali-pc>
2016-07-14 22:14:05 +0800

    update

--- gs-rest-service-complete/src/main/java/hello/Application.java
+++ gs-rest-service-complete/src/main/java/hello/Application.java
@@ -1,5 +1,6 @@
 package hello;
 
+/* Application */
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
